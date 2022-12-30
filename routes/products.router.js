f9de0e7c25912d50b9f4c870d08e3a20cd2ae62c
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2022-12-30 11:42:56 -0300

    adding patch and delete methods

--- routes/products.router.js
+++ routes/products.router.js
@@ -38,5 +38,24 @@ router.post('/', (req, res) => {
   const body = req.body
   res.json({message: 'created', data: body})
 })
+// products patch
+router.patch('/:id', (req, res) => {
+  const { id } = req.params;
+  const body = req.body
+  res.json({
+    message: 'update',
+    data: body,
+    id,
+  })
+})
+
+// products delete
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  res.json({
+    message: 'delete',
+    id,
+  })
+})
 
 module.exports = router;
