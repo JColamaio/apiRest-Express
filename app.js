cc9c98f715c9d61219e2ef3a925f42671cf99b40
Jonathan <jonathan_colamaio@solutionbox.com.ar>
2022-12-30 09:46:32 -0300

    adding app.routes

--- app.js
+++ app.js
@@ -6,18 +6,41 @@ const port = 3000;
 app.get('/', (req,res) => {
 res.send("hello world")
 })
-
+// new route
 app.get('/new-route', (req, res) => {
   res.send('im a new route')
 })
-
+//  products route
 app.get('/products', (req, res) => {
-  res.json({
-    name: 'Product1',
-    price: 1000
+  res.json([
+    {
+      name: 'Product 1',
+      price: 1000
+    },
+    {
+      name: 'Product 2',
+      price: 1500
+    }
+  ])
+})
+// ProductsId route
+app.get('/products/:id',(req, res) => {
+  const { id } = req.params.id
+  res.json( {
+    id,
+    name: 'Product 2',
+    price: 1500
   })
 })
-
+// categories and products route
+app.get('/categories/:categoryId/products/:productId', (req, res) => {
+  const { categoryId, productId } = req.params;
+  res.json({
+    categoryId,
+    productId
+  })
+} )
+// app listening on port
 app.listen(port, () => {
   console.log("server running on " + port);
 });
